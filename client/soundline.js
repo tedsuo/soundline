82d23094f17083a924a869c2090c0a6262ea0c9e
tedsuo <ted@radicaldesigns.org>
2012-08-10 21:56:49 +0200

    OAUTH_URL

--- client/soundline.js
+++ client/soundline.js
@@ -1,4 +1,5 @@
 // SoundLine namespace and constants
 SL = {
-  CLIENT_ID:  '0b4d05b2603939dff6ebf1f128ae7f1c'
+  CLIENT_ID:  '0b4d05b2603939dff6ebf1f128ae7f1c',
+  OAUTH_URL: 'http://soundline.tedsuo.com/oauth'
 };
