c1bab52c371e301da09d815fa99c8cb67b14948d
tedsuo <ted@radicaldesigns.org>
2012-08-11 12:23:19 +0200

    adding view namespace to SL

--- client/config.js
+++ client/config.js
@@ -1,5 +1,8 @@
-// SoundLine namespace and constants
-SL = {
+// SoundLine namespace and configuration 
+var SL = {
+  // Constants
   CLIENT_ID:  '0b4d05b2603939dff6ebf1f128ae7f1c',
-  OAUTH_URL: 'http://soundline.tedsuo.com/oauth'
+  OAUTH_URL: 'http://soundline.tedsuo.com/oauth',
+  // instance namespaces
+  v: {}
 };
