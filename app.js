640dc603d7c8ca98bcad3ac6291b521aa2a67b10
tedsuo <ted@radicaldesigns.org>
2012-08-11 22:08:01 +0200

    redis

--- app.js
+++ app.js
@@ -1,22 +1,48 @@
-// Dependencies
+/*
+ * DEPENDENCIES
+ */
 var express = require('express');
 var stitchit = require('stitchit');
+var redis = require('redis');
+var bufferRequest = require('./lib/buffer_request');
 
-// Constants
+/*
+ * CONSTANTS
+ */
 var PORT = process.env.PORT || 80;
 var PUBLIC_DIR = __dirname + '/client';
 var TEMPLATE_PATH = PUBLIC_DIR + '/templates/';
 var TEMPLATE_NAMESPACE = 'SL.t';
 
-// SoundLine http server
+
+/*
+ * SETUP
+ */
+
+// redis connection
+var db, redis_config;
+if (process.env.REDISTOGO_URL) {
+  redis_config = require("url").parse(process.env.REDISTOGO_URL);
+  db = redis.createClient(redis_config.port, redis_config.hostname);
+  db.auth(redis_config.auth.split(":")[1]);
+} else { 
+  db = redis.createClient();
+}
+
+// http server
 app = express();
+// serve up all the static files, no minification or gzipping
+app.use(express.static(PUBLIC_DIR));
+// start listening once everything is set up
 process.nextTick(function(){
   app.listen(PORT);
   console.log('SoundLine server started on port '+PORT);
 });
 
-// serve up all the static files, no minification or gzipping
-app.use(express.static(PUBLIC_DIR));
+
+/*
+ * ROUTES
+ */
 
 // compile all client side templates into a single namespace
 app.get('/templates',function(req,res){
@@ -26,6 +52,7 @@ app.get('/templates',function(req,res){
   });
 });
 
+// oauth2 response handler
 app.get('/oauth',function(req,res){
   res.send('<script src="//connect.soundcloud.com/sdk.js"></script>');
 });
