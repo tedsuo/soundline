b27f21afeb78c19c7210c6a738753dcc947a9e39
tedsuo <ted@radicaldesigns.org>
2012-08-11 22:09:18 +0200

    basic user model

---
+++ client/models/user.js
@@ -0,0 +1,55 @@
+SL.User = Backbone.Model.extend({
+  urlRoot: '/users',
+
+  authenticate: function(){
+    var user = this;
+    SC.connect(function() {
+      SL.setAccessToken(SC.storage().getItem('SC.accessToken'));
+      user.initializeFromSoundClound();
+    });
+  },
+ 
+  isLoggedIn: function(){
+    return SL.initializeAccessToken();
+  },
+
+  logout: function(){
+    SL.setAccessToken(null);
+  },
+
+  initializeFromSoundClound: function(){
+    var user = this;
+    SC.get('/me', function(sc_user) {
+      user.initializeFromServer(sc_user);
+    });
+  },
+  
+  initializeFromServer: function(sc_user){
+    var user = this;
+    var desired_fields = ['id','username','avatar_url'];
+    var user_data = _.pick(sc_user,desired_fields);
+    user.save(user_data,{
+      success: function(){
+        user.trigger('initialized');
+      },
+      error: function(){
+        console.log('init failure');
+      }
+    });
+  }
+
+},
+  // Static Methods
+{
+  findById: function(id,cb){
+    var user = new SL.User({id:id});
+    user.fetch({
+      success: function(model){
+        cb(null,model);
+      },
+      error: function(model,response){
+        cb( new Error('cannot find user'));
+      }
+    });
+  } 
+});
--- client/test/user.js
+++ client/test/user.js
@@ -1,4 +1,18 @@
 describe('User',function(){
-  it('should get a user by id',function(){
+  describe('initializeFromServer',function(done){
+    it('should save the userdata', function(done){
+      var user_id = _.uniqueId();
+      var user_name = 'test_'+user_id;
+      var user = new SL.User();
+      user.on('initialized',function(){
+        SL.User.findById( user_id, function(err,u){
+          if(err) return done(err);
+          u.get('id').should.equal(user_id);
+          u.get('username').should.equal(user_name);
+          done();
+        });
+      });
+      user.initializeFromServer({id:user_id, username:user_name});
+    });
   });
 });
