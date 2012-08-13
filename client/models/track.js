61cb13067d0b75a90342d05e3cd563e0fef42adc
tedsuo <ted@radicaldesigns.org>
2012-08-14 00:36:31 +0200

    basic track model

--- client/models/track.js
+++ client/models/track.js
@@ -1,5 +1,8 @@
 SL.Track = Backbone.Model.extend({
+  urlRoot: function(){
+    return this.get('playlist_id')+'/tracks';
+  },
   initialize: function(){
-    
+    if(!this.has('id')) this.set({ 'id': SL.uid() });
   }
 });
