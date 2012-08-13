3bf523d408a7919f8aee81dc84b61fa6f4ca1857
tedsuo <ted@radicaldesigns.org>
2012-08-14 00:37:06 +0200

    playlist api for fetching tracks from soundcloud

--- client/models/playlist.js
+++ client/models/playlist.js
@@ -1,3 +1,4 @@
+// custom events: add_tracks
 SL.Playlist = Backbone.Model.extend({
 
   urlRoot: function(){
@@ -6,7 +7,52 @@ SL.Playlist = Backbone.Model.extend({
 
   initialize: function(){
     if(!this.has('id')) this.set({ 'id': SL.uid() });
-    this.tracks = new SL.TrackList();
-  }
+    this.tracks = new SL.TrackList([],{ playlist: this, fetch:true });
+  },
+  
+  getTracks: function(){
+    return this.tracks;
+  },
+  
+  addTracks: function(items){
+    var tracks = this.tracks;
+    _.forEach(items,function(item){
+      tracks.createFromSoundcloud(item);
+    });
+    this.trigger('add_tracks');
+  },
 
+  addTracksFromUrl: function(url){
+    var playlist = this;
+    SC.get('/resolve',{url:url},function(results){
+      // whatever it is, if it has tracks, we want them
+      if(_.isObject(results) && _.isArray(results.tracks)){
+        results = results.tracks;
+      }
+
+      // kind of assuming the only array-like thing a url resolves too is a track list
+      if(_.isArray(results)){
+        playlist.addTracks(results);
+      } else { 
+        playlist.addTracksFromSoundcloudObject(results);
+      }
+    });
+  },
+
+  addTracksFromSoundcloudObject: function(result){
+    if(!_.isObject(result)) return;
+    switch(result.kind){
+      case 'user':
+        this.addTracksFromSoundcloudUser(result);
+        break;
+    }
+  },
+
+  addTracksFromSoundcloudUser: function(user){
+    var playlist = this;
+    var tracks = this.tracks;
+    SC.get('/users/'+user.id+'/tracks',function(results){
+      playlist.addTracks(results);
+    });
+  }
 });
