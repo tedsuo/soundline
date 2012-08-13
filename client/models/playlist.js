121ddf1a1129709ae05acc3ba856e93c6957505b
tedsuo <ted@radicaldesigns.org>
2012-08-13 17:41:40 +0200

    playlist urlRoot

--- client/models/playlist.js
+++ client/models/playlist.js
@@ -1,5 +1,9 @@
 SL.Playlist = Backbone.Model.extend({
 
+  urlRoot: function(){
+    return SL.current_user.id+'/playlists';
+  },
+
   initialize: function(){
     if(!this.has('id')) this.set({ 'id': SL.uid() });
     this.tracks = new SL.TrackList();
