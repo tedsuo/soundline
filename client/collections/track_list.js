3ac35838c7e6ca56a41163c6b49ee02b3d506582
tedsuo <ted@radicaldesigns.org>
2012-08-14 00:33:04 +0200

    basic track list

--- client/collections/track_list.js
+++ client/collections/track_list.js
@@ -1,8 +1,27 @@
 SL.TrackList = Backbone.Collection.extend({
   model: SL.Track,
 
-  urlRoot: function(){
-    return '/playlists/'+this.id;
-  }
+  url: function(){
+    return this.playlist.id+'/tracks';
+  },
 
+  initialize: function(m,o){
+    this.playlist = o.playlist;
+    if(o.fetch) this.fetch();
+  },
+
+  createFromSoundcloud:function(item,options){
+    if(!_.isObject(item) || item.kind !== 'track') return;
+    this.create({
+      playlist_id: this.playlist.id,
+      sc_id: item.id,
+      title: item.title,
+      duration: item.duration,
+      permalink_url: item.permalink_url,
+      stream_url: item.stream_url,
+      username: item.user.username,
+      user_url: item.user.permalink_url,
+      user_id: item.user_id
+    },options);
+  }
 });
