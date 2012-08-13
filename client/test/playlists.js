031538324174f7cca52a93a293bbd32cc2d9be70
tedsuo <ted@radicaldesigns.org>
2012-08-13 17:44:49 +0200

    testing PlaylistList#destroyByCid

--- client/test/playlists.js
+++ client/test/playlists.js
@@ -15,10 +15,31 @@ describe('Playlists', function(){
         },
         error: function(collection,resp){
           console.log(resp);
-          done(err);
+          done(new Error('fail'));
         }
       });
     });
     playlists.create({name:'foo',description:'bar'});
+  });
+ 
+  it('should destroy playlists by cid', function(done){
+    playlists.create({name:'first',description:'bar'});
+    playlists.create({name:'second',description:'bar'});
+    var cid = playlists.at(0).cid;
+    playlists.on('sync',function(){
+      var new_playlists = new SL.PlaylistList();
+      new_playlists.fetch({
+        success: function(){
+          new_playlists.models.length.should.equal(1);
+          new_playlists.models[0].get('name').should.equal('second');
+          done();
+        },
+        error: function(collection,resp){
+          console.log(resp);
+          done(new Error('fail'));
+        }
+      });
+    });
+    playlists.destroyByCid(cid);
   })
 });
