953940219db67927eacba201b4d4a73cdd7095e8
tedsuo <ted@radicaldesigns.org>
2012-08-14 00:32:37 +0200

    active playlist api for playlist_list

--- client/collections/playlist_list.js
+++ client/collections/playlist_list.js
@@ -1,13 +1,40 @@
 // is it really called that? yes. yes it is.
+// Custom events: set_active
 SL.PlaylistList = Backbone.Collection.extend({
   model: SL.Playlist,
 
   url: function(){
     return SL.current_user.id+'/playlists';
   },
-
+  
+  initialize: function(){
+    this.on('destroy', function(playlist){
+      if(playlist.cid == this.getActiveCid()) this.clearActive();
+    });
+  },
   destroyByCid: function(cid,o){
     var model = this.getByCid(cid);
     if(model) model.destroy();
+  },
+
+  setActive: function(cid){
+    var p = this.getByCid(cid);
+    if(p && (p.cid !== this.getActiveCid())){
+      this.active = p;
+      this.trigger('set_active');
+    }
+  },
+  
+  clearActive: function(){
+    this.active = null;
+    this.trigger('set_active');
+  },
+
+  getActive: function(){
+    return this.active;
+  },
+  
+  getActiveCid: function(){
+    if(this.active) return this.active.cid;
   }
 });
