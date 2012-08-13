caa44a4cedad1744c7b8c1bc5adc31e621137609
tedsuo <ted@radicaldesigns.org>
2012-08-13 17:45:13 +0200

    new playlist view

--- client/views/new_playlist.js
+++ client/views/new_playlist.js
@@ -1,6 +1,18 @@
 SL.NewPlaylistView = Backbone.View.extend({ 
   events: {
-    'click .js-remove': 'remove'
+    'click .js-remove': 'remove',
+    'click .js-submit': 'submit',
+    'submit form': 'submit'
+  },
+  
+  initialize: function(o){
+    this.playlists = o.playlists;
+  },
+
+  submit: function(){
+    var params = SL.formToParams($('form',this.$el));
+    this.playlists.create(params);
+    this.remove();
   },
 
   render: function(){
