6148623d2a1b886a74d714c6e3a47eea056a0a92
tedsuo <ted@radicaldesigns.org>
2012-08-18 20:04:20 +0200

    adding controls view and playlists collection to player view

--- client/views/player.js
+++ client/views/player.js
@@ -4,7 +4,8 @@ SL.PlayerView = Backbone.View.extend({
   },
   
   initialize: function(o){
-    this.playlists = this.model.getPlaylists();
+    this.player = this.model || o.player;
+    this.playlists = this.player.getPlaylists();
     this.playlists.on('set_active',this.showTracks,this);
     
     this.playlists_view = new SL.PlaylistListView({
@@ -13,14 +14,22 @@ SL.PlayerView = Backbone.View.extend({
     this.tracks_view = new SL.TracklistView({
       playlist: this.playlists.getActive() 
     });
+    this.controls_view = new SL.ControlsView({
+      playlists: this.playlists
+    });
   },
 
   render: function(){
     this.$el.html(SL.t.player());
-    this.playlists_view.setElement(document.getElementById('sidebar'));
-    this.playlists_view.render();
-    this.tracks_view.setElement(document.getElementById('main'));
-    this.tracks_view.render();
+    this.playlists_view
+      .setElement(document.getElementById('sidebar'))
+      .render();
+    this.tracks_view
+      .setElement(document.getElementById('main'))
+      .render();
+    this.controls_view
+      .setElement(document.getElementById('current-track'))
+      .render();
     return this;
   },
   
