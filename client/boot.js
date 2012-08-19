cbe747cdf1e8bf9a72525c47230c714a559b58e1
tedsuo <ted@radicaldesigns.org>
2012-08-19 09:12:35 +0200

    add left and right keyboard bindings for track control

--- client/boot.js
+++ client/boot.js
@@ -18,6 +18,17 @@ SL.boot = function(){
     SL.playlists = new SL.PlaylistList();
     SL.playlists.fetch();
 
+    $('html').on('keydown',function(event){
+      switch(event.which){
+        case SL.KEYPRESS_PREV:
+          SL.playlists.prevTrack();
+          return false;
+        case SL.KEYPRESS_NEXT:
+          SL.playlists.nextTrack();
+          return false;
+      }
+    });
+
     // setup music player
     SL.player = new SL.Player({
       playlists: SL.playlists
