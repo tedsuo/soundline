c65b34aa3304e1583b74f8dc566329caa437462b
tedsuo <ted@radicaldesigns.org>
2012-08-19 09:54:52 +0200

    basic play controls for track

--- client/models/track.js
+++ client/models/track.js
@@ -5,6 +5,7 @@ SL.Track = Backbone.Model.extend({
   initialize: function(){
     if(!this.has('id')) this.set({ 'id': SL.uid() });
     this.active = false;
+    this.is_playing = false;
   },
   isActive: function(){
     return this.active;
@@ -14,5 +15,24 @@ SL.Track = Backbone.Model.extend({
   },
   deactivate: function(){
     this.active =  false;
+  },
+  isPlaying: function(){
+    return this.is_playing;
+  },
+  playToggle: function(){
+    if(this.is_playing) this.pause();
+    else this.play();
+  },
+  play: function(){
+    if(this.is_playing === true) return;
+    this.is_playing = true;
+    console.log('playing');
+    this.trigger('play',this);
+  },
+  pause: function(){
+    if(this.is_playing === false) return;
+    this.is_playing = false;
+    console.log('paused');
+    this.trigger('pause',this);
   }
 });
