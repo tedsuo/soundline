70a96b1ca38c5f552241aca69c73de8db78b85f0
tedsuo <ted@radicaldesigns.org>
2012-08-20 17:44:29 +0200

    loading bar and fail message

--- client/views/track_list.js
+++ client/views/track_list.js
@@ -16,13 +16,46 @@ SL.TracklistView = Backbone.View.extend({
 
   initialize: function(){
     // note: playlist can only be set via setPlaylist
-    this.playlist = null;  
+    this.playlist = null;
+    this.loading_bar_timer_id =null;
   },
   
   render: function(){
     this.$el.html( SL.t.track_list(this.getData()) );
+    this.$loader = $('#track-loader',this.$el);
+    this.$loading_bar = $('#track-loading-bar',this.$el);
+    this.$fail_msg = $('.fail-msg',this.$el);
   },
 
+  showFailMsg: function(){
+    this.stopLoader();
+    this.$fail_msg.html(SL.t.add_track_fail());
+    this.$fail_msg.show();    
+  },
+  
+  startLoader: function(){
+    var view = this;
+    this.$fail_msg.hide();
+    this.$loader.show();
+    
+    var tick = 2;
+    var speed = 10;
+    var stop_position = 95;
+    this.loading_bar_timer_id = setInterval(function(){
+      tick = tick + (speed / tick);
+      if(tick > stop_position){
+        clearInterval(view.progress_timer_id);
+      } else {
+        view.$loading_bar.css({width: tick+'%'});
+      }
+    },0);
+  },
+  
+  stopLoader: function(){
+    this.$loader.hide();
+    clearInterval(this.loading_bar_timer_id);
+  },
+    
   getData: function(){
     if(this.playlist){
       return {
@@ -54,14 +87,19 @@ SL.TracklistView = Backbone.View.extend({
     this.playlist.on('add_tracks',this.render,this);
     this.playlist.on('change_track',this.render,this);
     this.playlist.on('remove_track',this.render,this);
+    this.playlist.on('add_tracks_failed',this.showFailMsg,this);
     this.render();
   },
 
   newTrack: function(){
     // get the url
     var url = $('.js-track-url-field',this.$el).val();
+    
+    if(_.isEmpty(url)) return false;
+    
     // add the tracks
-    if(!_.isEmpty(url)) this.playlist.addTracksFromUrl(url);
+    this.playlist.addTracksFromUrl(url);
+    this.startLoader();
     
     return false;
   },
